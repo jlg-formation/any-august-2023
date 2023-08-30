a5e46ad28e0ce42084b28639f2e5d852b16bda86
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-08-30 16:18:06 +0200

    dev: add an article test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,7 +1,52 @@
-describe("template spec", () => {
-  it("passes", () => {
+describe("Gestion Stock", () => {
+  it("home page", () => {
     cy.visit("https://jlg-formation.github.io/cypress-gestion-stock/");
 
     cy.contains("Gestion Stock");
+    cy.contains("Gérer efficacement votre stock");
+    cy.contains("footer", "Mentions Légales", {
+      timeout: 1000,
+    });
+
+    cy.get("a.button.primary");
+  });
+
+  it.only("should add an article", () => {
+    cy.visit("https://jlg-formation.github.io/cypress-gestion-stock/");
+
+    cy.contains("a.button.primary", "Voir le stock").click();
+
+    cy.url().should(
+      "eq",
+      "https://jlg-formation.github.io/cypress-gestion-stock/stock"
+    );
+
+    cy.get("a.button[title='Ajouter']").click();
+
+    cy.url().should(
+      "eq",
+      "https://jlg-formation.github.io/cypress-gestion-stock/stock/add"
+    );
+
+    const uuid = () => Cypress._.random(0, 1e6);
+    const id = uuid();
+    const testname = `o-${id}`;
+
+    cy.get("input").eq(0).clear().type(testname);
+    cy.get("input").eq(1).clear().type("12.34");
+
+    cy.get("input[type=range]").invoke("val", 34).trigger("change");
+
+    cy.contains("div.radio label", "Occasion").click();
+
+    cy.get("select").select("Alimentation");
+
+    cy.contains("div.radio span", "Périssable").click();
+
+    cy.get("input[type='date']").type("2023-09-30");
+
+    cy.contains("button", "Ajouter").click();
+
+    cy.contains("table tbody tr td", testname);
   });
 });
