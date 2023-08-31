4210b80bd5362a22bc4436740539dbf4555e3227
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-08-31 15:06:15 +0200

    dev: perimeter of spec files

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,6 +5,8 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    specPattern: ["cypress/e2e/**/*.cy.ts"],
+    excludeSpecPattern: ["cypress/e2e/**/*a2.cy.ts"],
   },
   video: true,
 });
